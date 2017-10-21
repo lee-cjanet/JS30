4214dfed9d2d8a4a6d82d95f40c152256bc8b388
Janet Lee <lee.cjanet@gmail.com>
2017-10-20 18:33:27 -0700

    one sound html and eventlistener created

---
+++ 1-DrumKit/main.js
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+</head>
+
+<body>
+
+<div class='keys'>
+  <div data-key='65' class='key'>
+    <kbd>A</kbd>
+    <span class='sound'>clap</span>
+  </div>
+</div>
+
+<audio data-key="65" src="sounds/clap.wav"></audio>
+
+<script>
+  window.addEventListener('keydown', function(e) {
+    console.log(e)
+    const audio = document.querySelector(`audio[data-key='${keyCode}']`);
+    if(!audio) return; //stop the func from running altogether
+    audio.currentTime = 0; //resets audio to beginning 0th second. audio normally wont play if there's already an audio playing
+    audio.play
+  });
+</script>
+
+</body>
+
+</html>
